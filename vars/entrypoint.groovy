944d241f2a2fa55403748380e496d4a351aa1ce1
timski <tklopotoski@ebsco.com>
2019-09-11 16:44:23 -0400

    Add entrypoint

---
+++ vars/entrypoint.groovy
@@ -0,0 +1,16 @@
+def call() {
+
+    library(
+        identifier: "child@master",
+        retriever: modernSCM(
+            github(
+                credentialsId: 'github-shared-secret',
+                repoOwner: 'skinitimski',
+                repository: 'library.child'
+            )
+        )
+    )
+
+    stepFromChild()
+}
+
